a30a9df2c0128c7a994f98aecb54737209fb8a82
Sacha Silvia <149610520+sachasilvia@users.noreply.github.com>
2024-04-11 13:57:36 -0400

    fix

--- dst_checker.ino
+++ dst_checker.ino
@@ -6,6 +6,7 @@ int wday;
 int month;
 int day;
 int previousSunday;
+int firstSunday;
 void setup() {
   Serial.begin(115200);
 }
@@ -14,10 +15,11 @@ void loop() {
   day = xxx;    // <-- Enter day here (1-31)
   wday = xxx;   // <-- Enter weekday here (0-6, where 0 = Sunday)
   previousSunday = day - wday;
+  firstSunday = wday - day;
   if (((month > march) && (month < november)) || 
   ((month == march) && (previousSunday >= 8)) || 
   ((month == march) && (day > 14)) || 
-  ((month == november) && (previousSunday <= 1))) {
+  ((month == november) && (firstSunday >= 0) && (day <= 7))) {
     Serial.println("Is DST");
   } else {
     Serial.println("Not DST");
